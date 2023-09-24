adcc686519976ccf444fd65d71e883b4dde56e7d
Md Al Maruf <32615553+mdalmaruf@users.noreply.github.com>
2023-09-24 13:46:17 -0400

    Create SelectionSort.c

---
+++ Problems/SelectionSort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+// selection sort
+#include<stdlib.h>
+
+int main(){
+    
+    int array[]={20, 35, 2, 45, 12};
+    int i,j;
+    
+    // printf("Enter the numbers\n");
+    // for(i=0; i<5; i++){
+    //     scanf("%d", &array[i]);
+    // }
+   
+    int size = sizeof(array)/sizeof(array[0]);
+    
+    printf("\nSize of the Array= %d", size);
+    int min;
+    for (i=0; i<size-1; i++){
+        min=i;
+        for(j=i+1;j<size; j++){
+            if(array[j]<array[min]){
+                min=j;
+            }
+            int temp = array[i];
+            array[i]=array[min];
+            array[min]=temp;
+            
+        }
+    }
+    
+    printf("\nPrint the numbers\n");
+    for(i=0; i<5; i++){
+        printf("%d ", array[i]);
+    }
+    
+    return 0;
+}
